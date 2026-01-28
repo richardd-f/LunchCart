60f047848e59b29e88c3711e86d861c5aea94a63
felix <ffelixrichardo@gmail.com>
2026-01-28 10:28:39 +0800

    fix midtrans isProduction in cart/component

--- src/features/cart/action.ts
+++ src/features/cart/action.ts
@@ -246,7 +246,7 @@ export async function createOrder(
     }
 
     const midtrans = new (require('midtrans-client').Snap)({
-        isProduction: false,
+        isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true',
         serverKey: process.env.MIDTRANS_SERVER_KEY,
         clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY,
     });
