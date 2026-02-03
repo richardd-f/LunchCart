a0fe45729373ea52cceaf29340a8f2377b8b4411
felix <ffelixrichardo@gmail.com>
2026-02-03 14:19:59 +0800

    set autoclose to 3 seconds for toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,7 +112,7 @@ export default function RootLayout({
         <SessionProvider>
           <ToastContainer
             position="top-center"
-            autoClose={5000}
+            autoClose={3000}
             hideProgressBar={false}
             newestOnTop={false}
             closeOnClick
